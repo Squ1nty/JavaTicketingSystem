6096102fd7bb11f6f6722f8150a7e0c645711dac
Vincent Dao <vincentdao288@gmail.com>
2025-03-13 13:42:42 +1100

    Completed terminal output up until first user input.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
 package src;
+
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args){
+    // Create a Scanner
+    Scanner input = new Scanner(System.in);
+
     // Welcoming user to the "Super Event" ticketing system
     System.out.println("===============================================");
     System.out.println("Welcome to Super Event!");
     System.out.println("===============================================");
+
+    // Prompting user to select an option
+    System.out.println("Choose an option:");
+
+    System.out.println("1. List all events");
+    System.out.println("2. Book a ticket");
+    System.out.println("3. View my booking");
+    System.out.println("4. Remove an event from the booking");
+    System.out.println("5. Checkout");
+    System.out.println("6. Quit");
+
+    System.out.print("Please enter a number to proceed: ");
+
+    int userChoice = input.nextInt();
+    // System.out.println(userChoice); Checking if code works so far
   }
 }
